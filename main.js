cdd34a444eecdc76d42ed002ef6050dc18c85804
Ivan Cio <ivanarturo.cio@gmail.com>
2023-01-18 22:11:20 -0600

    Se agrego el aside de carrito de compras al navbar

--- main.js
+++ main.js
@@ -2,15 +2,26 @@ const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burguerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 burguerMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 };
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+};
+
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
 };
